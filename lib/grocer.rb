8b53bfac446c5300d496be0dd4f9d13eb04f7708
Joshua Moore <morejoshua99@hotmail.com>
2020-07-08 10:10:29 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,7 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |k|
-if k[:item] == name 
-  return k
+    return nil unless k[:item] == name
 end
   end
 end
